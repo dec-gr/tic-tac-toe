b87f938a5ff5ff3f1348ef62eae046a7823cb149
Declan <144496393+dec-gr@users.noreply.github.com>
2023-09-20 15:37:49 +0100

    Create clickable board

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 // Module
 const gameBoard = (() => {
   let board = [
-    ["X", "O", ""],
-    ["", "O", "O"],
-    ["X", "O", "X"],
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
   ];
 
   const displayBoard = () => {
@@ -17,14 +17,13 @@ const gameBoard = (() => {
 
   const setBoard = (i, j, marker) => {
     const cell = document.getElementById(`${i}-${j}`);
+    board[i][j] = marker;
     cell.textContent = marker;
+    console.log(board);
   };
 
   // Check gameboard for winner
   // return player (or symbol) or tie
-
-  // Add to gameboard
-  // (Position + Player)
 
   return {
     displayBoard,
@@ -35,8 +34,44 @@ const gameBoard = (() => {
 // Factory
 const player = (name, marker) => {
   const displayMarker = () => console.log(marker);
-  return { name, displayMarker };
+  return { name, displayMarker, marker };
 };
 
 // Module
-const gameFlow = () => {};
+const gameFlow = (() => {
+  //startGame
+
+  //CreatePlayer1
+  player1 = player("Jeff", "X");
+
+  //CreatePlayer2
+  player2 = player("Jess", "O");
+
+  let currentPlayer = player1;
+
+  //DisplayBoard
+  gameBoard.displayBoard();
+
+  const tempFunction = (e) => {
+    if (e.textContent != "") return;
+    console.log(e);
+    let [i, j] = e.id.split("-");
+    gameBoard.setBoard(i, j, currentPlayer.marker);
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+  };
+
+  const gameCells = document.querySelectorAll(".game-cell");
+  gameCells.forEach((cell) => {
+    cell.addEventListener("click", function () {
+      let that = this;
+      tempFunction(that);
+    });
+  });
+
+  //   document.getElementById("1-1").addEventListener("click", function () {
+  //     let that = this;
+  //     tempFunction(that);
+  //   });
+
+  //CreatePlayer2
+})();
